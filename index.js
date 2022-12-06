bfe93acbc67e2281de72abe5aa4c6f329742af9a
claravbr <clara.vbr@gmail.com>
2022-12-06 20:21:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const fs=require("fs");
 const express = require('express');
 const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const modelo = require("./servidor/modelo.js");
+const sWS = require("./servidor/servidorWS.js");
 
 const PORT = process.env.PORT || 3000;
+//var args = process.argv.slice(2);
 
+// añadir juego
 let juego = new modelo.Juego();
+// objeto sWS
+let servidorWS=new sWS.ServidorWS();
 
 app.use(express.static(__dirname + "/"));
 
@@ -13,6 +24,17 @@ app.get("/", function(request,response){
   var contenido=fs.readFileSync(__dirname+"/cliente/index.html");
   response.setHeader("Content-type","text/html");
   response.send(contenido);
+});
+
+app.get("/comprobarUsuario/:nick", function(request, response){
+	let nick= request.params.nick;
+	let us=juego.obtenerUsuario(nick);
+	let res={"nick": -1};
+	if(us){
+		res.nick=us.nick;
+	}
+	response.send(res);
+
 });
 
 app.get("/agregarUsuario/:nick",function(request,response){
@@ -44,7 +66,37 @@ app.get("/obtenerPartidasDisponibles",function(request,response){
   response.send(lista);
 });
 
-app.listen(PORT, () => {
+app.get("/salir/:nick",function(request,response){
+  let nick=request.params.nick;
+  juego.usuarioSale(nick);
+  response.send({res:"ok"})
+})
+
+app.get("/abandonarPartida/:nick/:codigo", function(request, response){
+  let nick = request.params.nick;
+  let codigo = request.params.codigo;
+  juego.abandonarPartida(nick,codigo);
+  
+  response.send({res:"El usuario " + nick + " ha abandonado la partida " + codigo});
+});
+
+ app.get("/obtenerLogs", function(request,response){
+     juego.obtenerLogs(function(logs){
+       response.send(logs);
+	 });
+});
+
+// Start the server
+
+// app.listen(PORT, () => {
+//   console.log(`App está escuchando en el puerto ${PORT}`);
+//   console.log('Ctrl+C para salir');
+// });
+
+server.listen(PORT, () => {
   console.log(`App está escuchando en el puerto ${PORT}`);
   console.log('Ctrl+C para salir');
 });
+
+//lanzar el servidorWs
+servidorWS.lanzarServidorWS(io,juego);
