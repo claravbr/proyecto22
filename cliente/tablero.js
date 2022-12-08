fd2c74600600f10925bf319d9a49d69a39331300
claravbr <clara.vbr@gmail.com>
2022-12-08 23:30:38 +0100

    Correccion

--- cliente/tablero.js
+++ cliente/tablero.js
@@ -1,145 +1,162 @@
-function Tablero(size) {
+function Tablero() {
+    this.placingOnGrid = false;
+    this.nombreBarco = undefined;
+    this.flota;
+   
+    this.ini = function () {
+        
+        var humanCells = document.querySelector('.human-player').childNodes;
+        for (var k = 0; k < humanCells.length; k++) {
+            humanCells[k].self = this;
+            humanCells[k].addEventListener('click', this.placementListener, false);
+        }
+        var playerRoster = document.querySelector('.fleet-roster').querySelectorAll('li');
 
-	this.size = size;
-	this.nombreBarco;
-	this.placingOnGrid = false;
-	this.flota;
+        for (var i = 0; i < playerRoster.length; i++) {
+            playerRoster[i].self = this;
+            playerRoster[i].addEventListener('click', this.rosterListener, false);
+        }
 
-	this.mostrar = function (si) {
-		let x = document.getElementById("tablero");
-		if (si) {
-			x.style.display = "block";
-		}
-		else {
-			x.style.display = "none";
-		}
-	}
-	this.ini = function () {
-		var humanCells = document.querySelector('.human-player').childNodes;
-		for (var k = 0; k < humanCells.length; k++) {
-			humanCells[k].self = this;
-			humanCells[k].addEventListener('click', this.placementListener, false);
-			//humanCells[k].addEventListener('mouseover', this.placementMouseover, false);
-			//humanCells[k].addEventListener('mouseout', this.placementMouseout, false);
+        var computerCells = document.querySelector('.computer-player').childNodes;
+        for (var j = 0; j < computerCells.length; j++) {
+            computerCells[j].self = this;
+            computerCells[j].addEventListener('click', this.shootListener, false);
+        }
+    }
+    this.rosterListener = function (e) {
+        var self = e.target.self;
+        var roster = document.querySelectorAll('.fleet-roster li');
 
-			var computerCells = document.querySelector('.computer-player').childNodes;
-			for (var j = 0; j < computerCells.length; j++) {
-				computerCells[j].self = this;
-				computerCells[j].addEventListener('click', this.shootListener, false);
-			}
-		}
-	};
-	this.asignarFlotaListener = function () {
-		var playerRoster = document.querySelector('.fleet-roster').querySelectorAll('li');
-		for (var i = 0; i < playerRoster.length; i++) {
-			playerRoster[i].self = this;
-			playerRoster[i].addEventListener('click', this.rosterListener, false);
-		}
-	};
-	this.placementListener = function (e) {
-		self = e.target.self;
-		if (self.placingOnGrid) {
-			// Extract coordinates from event listener
-			var x = parseInt(e.target.getAttribute('data-x'), 10);
-			var y = parseInt(e.target.getAttribute('data-y'), 10);
-			self.colocarBarco(x, y, self.nombreBarco);
-		}
-	};
-	this.endPlacing = function (shipType) {
-		document.getElementById(shipType).setAttribute('class', 'placed');
-		self.placingOnGrid = false;
-	};
-	this.rosterListener = function (e) {
-		var self = e.target.self;
-		var cli = this;
-		// Remove all classes of 'placing' from the fleet roster first
-		var roster = document.querySelectorAll('.fleet-roster li');
-		for (var i = 0; i < roster.length; i++) {
-			var classes = roster[i].getAttribute('class') || '';
-			classes = classes.replace('placing', '');
-			roster[i].setAttribute('class', classes);
-		}
+        for (var i = 0; i < roster.length; i++) {
+            var classes = roster[i].getAttribute('class') || '';
+            classes = classes.replace('placing', '');
+            roster[i].setAttribute('class', classes);
+        }
 
-		// Set the class of the target ship to 'placing'
-		self.nombreBarco = e.target.getAttribute('id');
-		document.getElementById(self.nombreBarco).setAttribute('class', 'placing');
-		//Game.placeShipDirection = parseInt(document.getElementById('rotate-button').getAttribute('data-direction'), 10);
-		self.placingOnGrid = true;
-	};
-	this.colocarBarco = function (x, y, nombre) {
-		//comprobar limites
-		console.log("Colocar barco: " + x + "-" + y + " " + nombre);
-		cws.colocarBarco(nombre, x, y);
-		//return true;
-	};
-	this.terminarDeColocarBarco = function (barco, x, y) {
-		for (i = 0; i < barco.tam; i++) {
-			console.log("x: " + (x + i) + " y: " + y);
-			this.updateCell(x + i, y, "ship", 'human-player')
-		}
-		self.endPlacing(barco.nombre);
-	};
-	this.shootListener = function (e) {
-		var x = parseInt(e.target.getAttribute('data-x'), 10);
-		var y = parseInt(e.target.getAttribute('data-y'), 10);
-		console.log("disparo x: " + x + " y: " + y)
-		cws.disparar(x, y);
-	};
-	this.updateCell = function (x, y, type, target) {
-		var player = target; //'human-player';
-		var classes = ['grid-cell', 'grid-cell-' + x + '-' + y, 'grid-' + type];
-		document.querySelector('.' + player + ' .grid-cell-' + x + '-' + y).setAttribute('class', classes.join(' '));
-	};
-	this.createGrid = function () {
-		var gridDiv = document.querySelectorAll('.grid');
-		for (var grid = 0; grid < gridDiv.length; grid++) {
-			//gridDiv[grid].removeChild(gridDiv[grid].querySelector('.no-js')); // Removes the no-js warning
-			let myNode = gridDiv[grid];
-			while (myNode.lastElementChild) {
-				myNode.removeChild(myNode.lastElementChild);
-			}
-			for (var i = 0; i < this.size; i++) {
-				for (var j = 0; j < Game.size; j++) {
-					var el = document.createElement('div');
-					el.setAttribute('data-x', i);
-					el.setAttribute('data-y', j);
-					el.setAttribute('class', 'grid-cell grid-cell-' + i + '-' + j);
-					gridDiv[grid].appendChild(el);
-				}
-			}
-		}
-		this.ini();
-	};
+        self.nombreBarco = e.target.getAttribute('id');
+        document.getElementById(self.nombreBarco).setAttribute('class', 'placing');
+        //Game.placeShipDirection = parseInt(document.getElementById('rotate-button').getAttribute('data-direction'), 10);
+        self.placingOnGrid = true;
+    };
+    // Creates click event listeners on the human player's grid to handle
+    this.placementListener = function (e) {
+        var self = e.target.self;
+        if (self.placingOnGrid) {
+            // Extract coordinates from event listener
+            var x = parseInt(e.target.getAttribute('data-x'), 10);
+            var y = parseInt(e.target.getAttribute('data-y'), 10);
+            //console.log("Barco: " + self.nombreBarco + " x: " + x + " y: " + y);
+            // Don't screw up the direction if the user tries to place again.
+            //var successful = 
+            self.colocarBarco(self.nombreBarco, x, y);
+            //if (successful) {
+            //     // Done placing this ship
 
-	this.elementosGrid=function(){
-		$('#gc').remove();
-		let cadena='<div class="game-container" id="gc">';
-		cadena=cadena+'<div id="roster-sidebar">';
-	 	cadena=cadena+'<h4>Barcos</h4><div id="flota"></div></div><div class="grid-container"><h2>Tu flota</h2>';
-		cadena=cadena+'<div class="grid human-player"></div></div><div class="grid-container">';
-		cadena=cadena+'<h2>Flota enemiga</h2><div class="grid computer-player"></div></div></div>';
-		$('#ancla').append(cadena);
-		this.createGrid();
+            //self.endPlacing(Game.placeShipType);
+            //self.placingOnGrid = false;
 
-		$("#btnAyuda").on("click", function () {
-			iu.mostrarModal('<img src="cliente/img/ayuda.png"')
-		})
-	};
+            /*if (self.areAllShipsPlaced()) {
+                var el = document.getElementById('rotate-button');
+                el.addEventListener(transitionEndEventName(), (function () {
+                    el.setAttribute('class', 'hidden');
+                    if (gameTutorial.showTutorial) {
+                        document.getElementById('start-game').setAttribute('class', 'highlight');
+                    } else {
+                        document.getElementById('start-game').removeAttribute('class');
+                    }
+                }), false);
+                el.setAttribute('class', 'invisible');
+            } */
+            //}
+        }
+    };
 
-    this.mostrarFlota=function(){
-		$("#listaF").remove();
-		let cadena='<ul class="fleet-roster" id="listaF">';
-		for (let key in this.flota){
-			cadena=cadena+"<li id='"+key+"'>"+key+"</li>"
-		}
-		cadena=cadena+"</ul>";
-		$('#flota').append(cadena);
-		//<ul>
-		//	cadena=cadena+'<li id="b2">b2</li>'
-		//</ul>
-		this.asignarFlotaListener();
-	};
+    this.endPlacing = function (shipType) {
+        document.getElementById(shipType).setAttribute('class', 'placed');
 
-	//this.createGrid();
-    //this.mostrar(false);
+        // Wipe out the variable when you're done with it
+        //Game.placeShipDirection = null;
+        this.nombreBarco = '';
+        //Game.placeShipCoords = [];
+    };
+    this.colocarBarco = function (nombre, x, y) {
+        //console.log("Barco: " + nombre + " x: " + x + " y: " + y);
+        cws.colocarBarco(nombre, x, y);
+        cws.barcosDesplegados();
+        //return true;
+    }
+
+    this.disparo = function (x, y) {
+        cws.disparar(x, y);
+    }
+    this.puedesDisparar = function (estado, x, y, targetPlayer) {
+
+        if (estado == 'agua') {
+            this.updateCell(x, y, 'agua', targetPlayer);
+        } else if (estado == 'tocado') {
+            this.updateCell(x, y, 'tocado', targetPlayer);
+
+        } else {
+            this.updateCell(x, y, 'hundido', targetPlayer);
+        }
+        this.placingOnGrid = false;
+    };
+
+    this.shootListener = function (e) {
+        var self = e.target.self;
+        // Extract coordinates from event listener
+        var x = parseInt(e.target.getAttribute('data-x'), 10);
+        var y = parseInt(e.target.getAttribute('data-y'), 10);
+
+
+        self.disparo(x, y);
+    };
+    this.mostrarTablero = function () {
+
+    }
+    this.puedesColocarBarco = function (barco, x, y, desplegados) {
+
+        for (i = 0; i < barco.tam; i++) {
+            this.updateCell(x + i, y, 'ship', 'human-player');
+        }
+        this.placingOnGrid = false;
+        this.endPlacing(barco.nombre);
+
+
+
+    }
+    // Updates the cell's CSS class based on the type passed in
+    this.updateCell = function (x, y, type, targetPlayer) {
+        var player = targetPlayer;
+        var classes = ['grid-cell', 'grid-cell-' + x + '-' + y, 'grid-' + type];
+        document.querySelector('.' + player + ' .grid-cell-' + x + '-' + y).setAttribute('class', classes.join(' '));
+    };
+    this.crearGrid = function () {
+        var gridDiv = document.querySelectorAll('.grid');
+        for (var grid = 0; grid < gridDiv.length; grid++) {
+            //gridDiv[grid].removeChild(gridDiv[grid].querySelector('.no-js')); // Removes the no-js warning
+            for (var i = 0; i < 10; i++) {
+                for (var j = 0; j < 10; j++) {
+                    var el = document.createElement('div');
+                    el.setAttribute('data-x', j);
+                    el.setAttribute('data-y', i);
+                    el.setAttribute('class', 'grid-cell grid-cell-' + j + '-' + i);
+                    gridDiv[grid].appendChild(el);
+                }
+            }
+        }
+    }
+    this.mostrarTablero = function(flag){
+        let tab = document.getElementById("tablero");
+        if (flag) {
+            tab.style.display = "block";
+        } else {
+            tab.style.display = "none";
+        }
+    };
+    //Constructor
+    this.crearGrid();
+    this.ini();
+
+
 }
--- servidor/servidorWS.js
+++ servidor/servidorWS.js
@@ -1,140 +1,156 @@
+
 function ServidorWS() {
-	//enviar peticiones
-	this.enviarAlRemitente = function (socket, mensaje, datos) {
-		socket.emit(mensaje, datos);
-	}
-	this.enviarATodosEnPartida = function (io, codigo, mensaje, datos) {
-		io.sockets.in(codigo).emit(mensaje, datos);
-	}
-	this.enviarATodos = function (socket, mens, datos) {
-		socket.broadcast.emit(mens, datos);
-	}
+    //peticiones
+    this.enviarAlRemitente = function (socket, mensaje, datos) {
+        socket.emit(mensaje, datos);
+    }
+    this.enviarATodosEnPartida = function (io, codigo, mensaje, datos) {
+        io.sockets.in(codigo).emit(mensaje, datos)
+    }
+    this.enviarATodos = function (socket, mensaje, datos) {
+        socket.broadcast.emit(mensaje, datos);
+    }
 
-	//gestionar peticiones
-	this.lanzarServidorWS = function (io, juego) {
-		let cli = this;
-		io.on('connection', (socket) => {
-			console.log('Usuario conectado');
-			socket.on("crearPartida", function (nick) {
-				let res = juego.jugadorCreaPartida(nick);
-				let codigoStr = res.codigo.toString();
-				socket.join(codigoStr);
-				cli.enviarAlRemitente(socket, "esperandoRival", res);
-				cli.enviarATodosEnPartida(io, codigoStr, "partidaCreada", res)
-				let lista = juego.obtenerPartidasDisponibles();
-				cli.enviarATodos(socket, "actualizarListaPartidas", lista);
-			});
-			socket.on("unirseAPartida", function (nick, codigo) {
-				let codigoStr = codigo.toString();
-				socket.join(codigoStr);
-				let res = juego.jugadorSeUneAPartida(nick, codigo);
-				cli.enviarAlRemitente(socket, "unidoAPartida", res);
+    //gestionar peticiones
+    this.lanzarServidorWS = function (io, juego) {
+        let cli = this;
+        io.on('connection', (socket) => {
+            console.log('Usuario conectado');
 
-				let partida = juego.obtenerPartida(codigo);
+            socket.on("crearPartida", function (nick) {
+                let res = juego.jugadorCreaPartida(nick);
+                let codigoStr = res.codigo.toString();
+                socket.join(codigoStr);
+                cli.enviarATodosEnPartida(io, codigoStr, "partidaCreada", res);
+                cli.enviarAlRemitente(socket, "esperandoRival", res);
+                let lista = juego.obtenerPartidasDisponibles();
+                cli.enviarATodos(socket, "actualizarListaPartidasDisp", lista);
+            });
+            socket.on("unirseAPartida", function (nick, codigo) {
+                let res = juego.jugadorSeUneAPartida(nick, codigo);
+                let codigoStr = res.codigo.toString();
+                socket.join(codigoStr);
+                cli.enviarAlRemitente(socket, "unidoAPartida", res);
 
-				if (partida.esDesplegando()) {
-					let usr = juego.obtenerUsuario(nick);
-					let flota = usr.obtenerFlota();
-					res = { flota: flota };
-					console.log(res.flota);
-					cli.enviarATodosEnPartida(io, codigo, "faseDesplegando", res);
-				}
-			});
-			socket.on("abandonarPartida", function (nick, codigo) {
-				let res = juego.obtenerPartida(codigo);
-				let codigoStr = res.codigo.toString();
-				let partida = juego.obtenerPartida(res.codigo);
+                let partida = juego.obtenerPartida(res.codigo);
 
-				res = { nick: nick, codigo: partida.codigo };
-				cli.enviarATodosEnPartida(io, codigoStr, "jugadorAbandona", res);
-				juego.abandonarPartida(nick, codigo);
-			});
+                if (partida.esDesplegando()) {
+                    let usr = juego.obtenerUsuario(nick);
+                    let flota = usr.obtenerFlota();
+                    res = { flota: flota };
+                    console.log(res.flota);
+                    cli.enviarATodosEnPartida(io, codigoStr, "aDesplegar", res);
+                }
+            });
+            socket.on("abandonarPartida", function (nick, codigo) {
+                let res = juego.obtenerPartida(codigo);
+                let codigoStr = res.codigo.toString();
+                let partida = juego.obtenerPartida(res.codigo);
 
-			socket.on("colocarBarco", function (nick, nombre, x, y) {
-				let user = juego.obtenerUsuario(nick);
-				if (user) {
-					if (user.partida) {
-
-						user.colocarBarco(nombre, x, y);
-
-						let partida = juego.obtenerPartida(user.partida.codigo);
-
-						let res = { nick: nick, barco: nombre, x: x, y: y, colocado: true };
-						cli.enviarAlRemitente(socket, "barcoColocado", res);
-						console.log("El Barco " + res.barco + " es colocado en la posición (" + res.x + "," + res.y + ")");
-					} else {
-						console.log("No se pueden desplegar barcos si no estás dentro de una partida.");
-						res = { estado: true };
-						cli.enviarAlRemitente(socket, "noColocar", res);
-					}
-				}
-			});
-
-			socket.on("barcosDesplegados", function (nick) {
-				let jugador = juego.obtenerUsuario(nick);
-				if (jugador) {
-					let partida = jugador.partida;
-					jugador.barcosDesplegados();
-					let codigoStr = partida.codigo.toString();
-					if (partida.esJugando()) {
-						let res = { fase: partida.fase, turno: partida.turno.nick };
-						cli.enviarATodosEnPartida(io, codigoStr, "aJugar", res);
-					}
-				}
-			});
-
-			socket.on("disparar", function (nick, x, y) {
-				let user = juego.obtenerUsuario(nick);
-				if (user && user.partida.esJugando()) {
-
-					let turno = user.partida.turno;
-					console.log("Turno de: " + turno.nick);
-					if (user.nick == turno.nick) {
-
-						user.disparar(x, y);
-						let partida = juego.obtenerPartida(user.partida.codigo);
-						let codigoStr = partida.codigo.toString();
-						let estado = user.obtenerEstadoMarcado(x, y);
-						if (estado == 'agua') {
-							rival = user.partida.obtenerRival(nick);
-							let res = { atacante: nick, atacado: rival.nick, turno: turno.nick, estado: 'agua', x: x, y: y }
-							//cli.enviarAlRemitente(socket, "disparo", res);
-							cli.enviarATodosEnPartida(io, codigoStr, "disparo", res);
-						}
-						else {
-							if (estado == 'tocado') {
-								let res = { atacante: nick, turno: turno.nick, estado: 'tocado', x: x, y: y };
-								cli.enviarATodosEnPartida(io, codigoStr, "disparo", res);
-								//cli.enviarAlRemitente(socket, "disparo", res);
-							}
-							else {
-								let res = { atacante: nick, turno: turno.nick, estado: 'hundido', x: x, y: y };
-								//cli.enviarAlRemitente(socket, "disparo", res);
-								cli.enviarATodosEnPartida(io, codigoStr, "disparo", res);
-							}
-						}
-
-						if (partida.esFinal()) {
-							let res = { turno: turno.nick };
-							cli.enviarATodosEnPartida(io, codigoStr, "finPartida", res);
-						}
-
-					}
-					else {
-						rival = user.partida.obtenerRival(nick);
-						let res = { turno: rival.nick };
-						cli.enviarAlRemitente(socket, "noEsTuTurno", res);
-					}
+                res = { nick: nick, codigo: partida.codigo };
+                cli.enviarATodosEnPartida(io, codigoStr, "userAbandona", res);
+                juego.abandonarPartida(nick, codigo);
 
 
-				}
-				else {
-					console.log("No se puede disparar hasta que se desplieguen todos los barcos.");
-					let res = { nick: nick };
-					cli.enviarAlRemitente(socket, "noDispare", res);
-				}
-			});
-			socket.on("salir", function (nick) {
+            });
+
+            socket.on("colocarBarco", function (nick, nombre, x, y) {
+
+                let user = juego.obtenerUsuario(nick);
+                if (user) {
+                    if (user.partida) {
+                         
+                        user.colocarBarco(nombre, x, y);
+                        
+                        let partida = juego.obtenerPartida(user.partida.codigo);
+                        
+                        let res = { nick: nick, barco: nombre, x: x, y: y, colocado: true };
+                        cli.enviarAlRemitente(socket, "barcoColocado", res);
+                        console.log("El Barco " + res.barco + " es colocado en la posición (" + res.x + "," + res.y + ")");
+
+                    }
+                    else {
+                        console.log("No se pueden desplegar barcos si no estás dentro de una partida.");
+                        res = { estado: true };
+                        cli.enviarAlRemitente(socket, "noColocar", res);
+                    }
+
+
+                }
+
+            });
+            socket.on("barcosDesplegados", function (nick) {
+
+                let user = juego.obtenerUsuario(nick);
+                if (user) {
+                    user.barcosDesplegados();
+                    console.log("Fase " + user.partida.fase);
+                    if (user.partida.esJugando()) {
+                        let res = { turno: user.partida.turno.nick };
+                        let codigoStr = user.partida.codigo.toString();
+                        cli.enviarATodosEnPartida(io, codigoStr, "aJugar", res);
+
+                        console.log("Barcos deplegados. Comienza a atacar: " + res.turno);
+
+                    }
+                    
+                }
+
+            });
+            socket.on("disparar", function (nick, x, y) {
+
+                let user = juego.obtenerUsuario(nick);
+                if (user && user.partida.esJugando()) {
+                    
+                    let turno = user.partida.turno;
+                    console.log("Turno de: " + turno.nick);
+                    if (user.nick == turno.nick) {
+
+                        user.disparar(x, y);
+                        let partida = juego.obtenerPartida(user.partida.codigo);
+                        let codigoStr = partida.codigo.toString();
+                        let estado = user.obtenerEstadoMarcado(x, y);
+                        if (estado == 'agua') {
+                            rival = user.partida.obtenerRival(nick);
+                            let res = { atacante: nick, atacado: rival.nick, turno: turno.nick, estado: 'agua', x: x, y: y }
+                            //cli.enviarAlRemitente(socket, "disparo", res);
+                            cli.enviarATodosEnPartida(io, codigoStr, "disparo", res);
+                        }
+                        else {
+                            if (estado == 'tocado') {
+                                let res = { atacante: nick, turno: turno.nick, estado: 'tocado', x: x, y: y };
+                                cli.enviarATodosEnPartida(io, codigoStr, "disparo", res);
+                                //cli.enviarAlRemitente(socket, "disparo", res);
+                            }
+                            else {
+                                let res = { atacante: nick, turno: turno.nick, estado: 'hundido', x: x, y: y };
+                                //cli.enviarAlRemitente(socket, "disparo", res);
+                                cli.enviarATodosEnPartida(io, codigoStr, "disparo", res);
+                            }
+                        }
+
+                        if (partida.esFinal()) {
+                            let res = { turno: turno.nick };
+                            cli.enviarATodosEnPartida(io, codigoStr, "finPartida", res);
+                        }
+
+                    }
+                    else {
+                        rival = user.partida.obtenerRival(nick);
+                        let res = { turno: rival.nick };
+                        cli.enviarAlRemitente(socket, "noEsTuTurno", res);
+                    }
+
+
+                }
+                else {
+                    console.log("No se puede disparar hasta que se desplieguen todos los barcos.");
+                    let res = { nick: nick };
+                    cli.enviarAlRemitente(socket, "noDispare", res);
+                }
+
+            });
+            socket.on("salir", function (nick) {
                 let user = juego.obtenerUsuario(nick);
                 //let partida = juego.obtenerPartida(user.partida.codigo);
                 if (!user.partida & !user.partida.fase == "final") {
@@ -152,8 +168,8 @@ function ServidorWS() {
                 };
 
             });
-		});
-	}
-}
+        });
 
+    }
+}
 module.exports.ServidorWS = ServidorWS;
